23d94d2b1f774163c071d8d4c3ee7fefb9e1dca9
Tran Quang Danh <danh.tran@novazone.co>
2025-07-03 17:25:18 +0700

    commit

---
+++ Ecommerce-CaFeShop/Areas/Admin/Controllers/BillController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DongHo_Admin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Policy = "Admin")]
+    public class BillController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+    
+}
